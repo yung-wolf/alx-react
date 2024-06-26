4133f16bab8b5522a65a57404f32191fa41c8b43
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-06-26 03:34:21 +0000

    Modify the App

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        {/* <img src={MyLogo} className="App-logo" alt="logo" /> */}
+        <div className="App-logo"></div>
+        <h1>
+          School dashboard
+        </h1>
+      </div>
+      <hr />
+
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" />
+        <button>OK</button>
+      </div>
+
+      <div className="App-footer">
+        <hr />
+        <p>
+          {getFooterCopy(true)}
+        </p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
