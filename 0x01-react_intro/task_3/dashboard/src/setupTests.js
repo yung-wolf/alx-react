96d9791c0fb5d9b95045073af41d53a7d54d36b6
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-06-26 15:09:55 +0000

    Import Enzyme

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,5 @@
 // enzyme.setup.js
-import { configure } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 Enzyme.configure({ adapter: new Adapter() });
