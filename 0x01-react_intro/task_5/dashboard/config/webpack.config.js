c82e441aa03a9a05a608d88e0ed38ecbea56154d
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-06-27 02:22:13 +0000

    Install Babel

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,28 +1,45 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
+  entry: './src/index.js',  // the entry file for our app
+  output: {  // the bundle output path and its filename
     filename: 'bundle.js',
-    clean: true,
+    path: path.resolve('./dist'),
   },
-  module: {
+  module: {  // where we tell webpack how to handle different file types
     rules: [
-      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
-      { test: /\.(png|svg|jpg|jpeg|gif)$/i, use: [
-        "file-loader",
-        {loader: "image-webpack-loader", options: { bypassOnDebug: true, disable: true }}
-      ] }
-    ]
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.join(__dirname, "public"),
-    port: 8564,
-    compress: true,
-    open: true,
+  mode: 'development',  // the app should work in either development or production mode
+  devtool: 'inline-source-map',  // use 'inline-source-map' if you are developing in a dev environment
+  devServer: {  // the webpack-dev-server configuration
     hot: true,
+    static: path.resolve('./dist'),
+    compress: true,
+    port: 3000,
   },
-}
+};
