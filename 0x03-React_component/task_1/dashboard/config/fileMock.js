4143609d3719c92fab82ddd72f35c9febfc68ab8
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-07-05 12:57:31 +0000

    Remove dashboard dir

--- 0x03-React_component/task_1/dashboard/config/fileMock.js
+++
@@ -1,3 +0,0 @@
-// A mock file for image imports, similar to the CSS mock.
-// This mock will return a dummy value (like a string) for any image import.
-module.exports = 'test-file-stub';
--- 0x03-React_component/task_1/dashboard/config/setupTests.js
+++
@@ -1,5 +0,0 @@
-// enzyme.setup.js
-import Enzyme from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
-
-Enzyme.configure({ adapter: new Adapter() });
