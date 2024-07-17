764fd63387250434f9e9c20ae12482e82070b230
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-07-17 20:04:06 +0000

    Create immutable List and push new element

---
+++ 0x06-React_Immutable/3-list.js
@@ -0,0 +1,10 @@
+import { List } from "immutable";
+
+export function getListObject(array) {
+  return List(array);
+}
+
+export function addElementToList(list, element) {
+  const newList = list.push(element);
+  return newList;
+}
