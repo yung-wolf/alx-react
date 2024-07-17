277f84db9ed495246b22e636c40eafeadfd96094
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-07-17 20:30:01 +0000

    Chained mutations

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,14 @@
+import { Map } from 'immutable';
+
+const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+}); 
+
+const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+
+export { map, map2 };
