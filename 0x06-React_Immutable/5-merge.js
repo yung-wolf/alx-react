84ac2dc48489662f3337860b025827becff6cab3
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-07-18 17:20:41 +0000

    Export functions

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,14 +1,12 @@
 import { List, Map } from 'immutable';
 // const { List, Map } = require('immutable');
 
-function concatElements (page1, page2) {
+export function concatElements (page1, page2) {
   const list = page1.concat(page2);
   return List(list);
 }
 
-function mergeElements (page1, page2) {
+export function mergeElements (page1, page2) {
   const list = page1.merge(page2);
   return Map(list);
 }
-
-module.exports = { concatElements, mergeElements }
