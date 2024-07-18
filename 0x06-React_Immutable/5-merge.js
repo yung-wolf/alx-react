a04eb384c26a762e30599f389581b45f1519a572
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-07-18 17:30:42 +0000

    Return list on mergeElements II

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -7,6 +7,6 @@ export function concatElements (page1, page2) {
 }
 
 export function mergeElements (page1, page2) {
-  const list = page1.merge(page2);
-  return Map(list).toList();
+  const list = page1.merge(page2).toList();
+  return Map(list);
 }
